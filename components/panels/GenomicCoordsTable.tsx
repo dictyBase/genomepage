928ac8efe1dadf4708efa9a3b3880c8ce2bd8f7f
Ben Bradley <ben.bradley747@gmail.com>
2022-10-19 16:50:25 -0500

    refactor: revert table components to @material-ui/core

--- components/panels/GenomicCoordsTable.tsx
+++ components/panels/GenomicCoordsTable.tsx
@@ -1,64 +1,74 @@
 import React from "react"
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from "@mui/material/Paper";
-import { GenomicCoordinates } from 'dicty-graphql-schema';
+import Table from "@material-ui/core/Table"
+import TableBody from "@material-ui/core/TableBody"
+import TableCell from "@material-ui/core/TableCell"
+import TableContainer from "@material-ui/core/TableContainer"
+import TableHead from "@material-ui/core/TableHead"
+import TableRow from "@material-ui/core/TableRow"
+import Paper from "@material-ui/core/Paper"
+import { GenomicCoordinates } from "dicty-graphql-schema"
 import { makeStyles } from "@material-ui/core/styles"
-import OtherError from "components/errors/OtherError";
+import OtherError from "components/errors/OtherError"
 
 type TableDisplayProps = {
-    /** Array of Genomic Coordinates for a particular gene */
-    data: GenomicCoordinates[] | null | undefined
+  /** Array of Genomic Coordinates for a particular gene */
+  data: GenomicCoordinates[] | null | undefined
 }
 
 const useStyles = makeStyles({
   root: {
-    display: 'flex',
+    display: "flex",
     marginTop: 30,
-    overflowX: 'hidden',
+    overflowX: "hidden",
   },
   table: {
     minWidth: 340,
   },
   tableCell: {
     paddingRight: 4,
-    paddingLeft: 5
-  }
+    paddingLeft: 5,
+  },
 })
-
 
 /**
  * Table Display component that display product info data
  */
 const TableDisplay = ({ data }: TableDisplayProps) => {
   const classes = useStyles()
-  if(!data) return <OtherError />
-
+  if (!data) return <OtherError />
 
   return (
     <TableContainer component={Paper} className={classes.root}>
-        <Table sx={{ minWidth: 100 }} aria-label="simple table" className={classes.table}>
+      <Table aria-label="simple table" className={classes.table}>
         <TableHead>
-            <TableRow>
-            <TableCell className={classes.tableCell} align="left">Exon</TableCell>
-            <TableCell className={classes.tableCell} align="left">Local Coords.</TableCell>
-            <TableCell className={classes.tableCell} align="left">Chrom. Coords.</TableCell>
-            </TableRow>
+          <TableRow>
+            <TableCell className={classes.tableCell} align="left">
+              Exon
+            </TableCell>
+            <TableCell className={classes.tableCell} align="left">
+              Local Coords.
+            </TableCell>
+            <TableCell className={classes.tableCell} align="left">
+              Chrom. Coords.
+            </TableCell>
+          </TableRow>
         </TableHead>
         <TableBody>
-        {data.map((item) => (
+          {data.map((item) => (
             <TableRow key={item.exon}>
-                <TableCell className={classes.tableCell} align="left">{item.exon}</TableCell>
-                <TableCell className={classes.tableCell} align="left">{item.local_coords}</TableCell>
-                <TableCell className={classes.tableCell} align="left">{item.chrom_coords}</TableCell>
+              <TableCell className={classes.tableCell} align="left">
+                {item.exon}
+              </TableCell>
+              <TableCell className={classes.tableCell} align="left">
+                {item.local_coords}
+              </TableCell>
+              <TableCell className={classes.tableCell} align="left">
+                {item.chrom_coords}
+              </TableCell>
             </TableRow>
-        ))}
+          ))}
         </TableBody>
-        </Table>
+      </Table>
     </TableContainer>
   )
 }
